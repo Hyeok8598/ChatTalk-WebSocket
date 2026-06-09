6cb0d6f82acf638472dae693fa79fb3341add512
Hyeok8598 <soonh8598@gmail.com>
2026-06-10 04:42:24 +0900

    Feature-009 : DB 연동 - EF Core 연동 - Npgsql 설정 - ChatTalkDbContext 생성 - Entity 생성 (ChatMessageEntity, UserEntity) - Migration 적용 - Snake Case 네이밍 규칙 적용 (ChatTalk.Common) - users / chat_messages 테이블 생성

---
+++ ChatTalk.Common/Utils/NamingConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ChatTalk.Common.Utils
+{
+    public static class NamingConverter
+    {
+        public static string ToSnakeCase(string text)
+        {
+            return Regex.Replace(text, "(?<!^)([A-Z])", "_$1").ToLower();
+        }
+    }
+}
--- ChatTalk.WebServer/Program.cs
+++ ChatTalk.WebServer/Program.cs
@@ -1,8 +1,17 @@
 using ChatTalk.WebServer;
+using ChatTalk.WebServer.Data;
 using ChatTalk.WebServer.Network;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+/* 26.06.10 - DB 연동 */
+builder.Services.AddDbContext<ChatTalkDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ChatTalkDb"));
+});
+
 var app = builder.Build();
 
 app.UseWebSockets();
