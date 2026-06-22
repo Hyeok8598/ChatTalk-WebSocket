237ffd93d982b28cf156a50e652c5c16055726b6
Hyeok8598 <soonh8598@gmail.com>
2026-06-22 09:43:58 +0900

    Feature-020 : 소켓 종료 로직 및 JOIN/LEAVE 처리 추가 > - JOIN/LEAVE 시스템 메시지 추가 > - WebSocket SYSTEM 메시지 처리 구조 추가

--- ChatTalk.WebServer/Network/Service/WebSocketReceiveService.cs
+++ ChatTalk.WebServer/Network/Service/WebSocketReceiveService.cs
@@ -9,56 +9,72 @@ namespace ChatTalk.WebServer.Network.Service
     {
         private readonly ILogger<WebSocketReceiveService> _logger;
         private readonly MessageDispatcherService _messageDispatcherService;
+        private readonly SessionService _sessionService;
 
-        public WebSocketReceiveService (ILogger<WebSocketReceiveService> logger, MessageDispatcherService messageDispatcherService)
+        public WebSocketReceiveService (ILogger<WebSocketReceiveService> logger, MessageDispatcherService messageDispatcherService, SessionService sessionService)
         {
             _logger = logger;
             _messageDispatcherService = messageDispatcherService;
+            _sessionService = sessionService;
         }
 
         public async Task ReceiveAsync(WebSocketHandler handler)
         {
             var buffer = new byte[1024];
 
-            while (handler.GetWebSocket().State == WebSocketState.Open)
+            try
             {
-                WebSocketReceiveResult receive = await handler.GetWebSocket().ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-                
-                if(receive.MessageType == WebSocketMessageType.Close)
+                while (handler.GetWebSocket().State == WebSocketState.Open)
                 {
-                    _logger.LogInformation(
-                        "[DISCONNECT] ConnectionId={ConnectionId}",
-                        handler.GetConnectId()
-                    );
+                    WebSocketReceiveResult receive = await handler.GetWebSocket().ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
 
-                    /* 소켓 종료 로직 구현 */
-                    return;
-                } 
-                else
-                {
-                    string json = Encoding.UTF8.GetString(buffer, 0, receive.Count);
-
-                    _logger.LogInformation(
-                        "[RECEIVE] ConnectionId={ConnectionId} Json={Json}",
-                        handler.GetConnectId(),
-                        json
-                    );
-
-                    BaseMessage? message = MessageConverter.Create(json);
-
-                    if (message == null)
+                    if (receive.MessageType == WebSocketMessageType.Close)
                     {
-                        _logger.LogWarning(
-                            "[INVALID_MESSAGE] Json={Json}",
+                        _logger.LogInformation(
+                            "[DISCONNECT] ConnectionId={ConnectionId}",
+                            handler.GetConnectId()
+                        );
+
+                        /* 소켓 종료 로직 구현 */
+                        return;
+                    }
+                    else
+                    {
+                        string json = Encoding.UTF8.GetString(buffer, 0, receive.Count);
+
+                        _logger.LogInformation(
+                            "[RECEIVE] ConnectionId={ConnectionId} Json={Json}",
+                            handler.GetConnectId(),
                             json
                         );
 
-                        return;
-                    }
+                        BaseMessage? message = MessageConverter.Create(json);
 
-                    await _messageDispatcherService.DispatcherAsync(handler, message);
+                        if (message == null)
+                        {
+                            _logger.LogWarning(
+                                "[INVALID_MESSAGE] Json={Json}",
+                                json
+                            );
+
+                            return;
+                        }
+
+                        await _messageDispatcherService.DispatcherAsync(handler, message);
+                    }
                 }
             }
-        }
+            catch (Exception ex)
+            {
+                _logger.LogWarning(ex, "[SOCKET DISCONNECT]");
+                _logger.LogInformation(
+                    "[RECEIVE] ConnectionId={ConnectionId}",
+                    handler.GetConnectId()
+                );
+            }
+            finally
+            {
+                await _sessionService.DisconnectAsync(handler);
+            }
     }
-}
+}}
